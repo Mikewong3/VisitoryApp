f844c9d9169fcfc1d5e48f7bad89040d9e57f1bf
Mike Wong <jwong3398@gmail.com>
2020-03-12 17:09:51 -0400

    Test to see if node_modules are added

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+
+const Client = require("@googlemaps/google-maps-services-js").Client;
+
+const client = new Client({});
+
+app.use(cors());
+app.get("/", function(req, res) {
+  client
+    .findPlaceFromText({
+      params: {
+        key: "AIzaSyDB0vvrNozsGFZD6lD97DTD3oKhtHTZRxk",
+        input: "Morton's The Steakhouse",
+        inputtype: "textquery"
+      }
+    })
+    .then(resp => {
+      console.log(resp.data.candidates);
+      res.send(resp.data.candidates);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+});
+// let testData = [{ name: "Mike" }];
+// app.get("/", function(req, res) {
+//   res.send(testData);
+//   console.log("Get called");
+// });
+app.listen(3000);

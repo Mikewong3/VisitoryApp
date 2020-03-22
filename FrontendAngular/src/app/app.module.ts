10097f71236a374fb329cf1bf197e2489595a926
Mike Wong <jwong3398@gmail.com>
2020-03-22 13:19:53 -0400

    Added a tutorial marker feature to agm map, reverted to beta 7 for agm

--- FrontendAngular/src/app/app.module.ts
+++ FrontendAngular/src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from "./app.component";
 import { SearchComponent } from "./search/search.component";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-import { GoogleMapComponent } from './google-map/google-map.component';
+import { GoogleMapComponent } from "./google-map/google-map.component";
+import { AgmCoreModule } from "@agm/core";
+import { GoogleMapsAPIWrapper } from "@agm/core/services";
 
 @NgModule({
   declarations: [AppComponent, SearchComponent, GoogleMapComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyDB0vvrNozsGFZD6lD97DTD3oKhtHTZRxk"
+    })
+  ],
+  providers: [GoogleMapsAPIWrapper],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- FrontendAngular/src/app/google-map/google-map.component.ts
+++ FrontendAngular/src/app/google-map/google-map.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild, NgZone, OnInit } from "@angular/core";
+import { MapsAPILoader, AgmMap } from "@agm/core";
+import { GoogleMapsAPIWrapper } from "@agm/core/services";
+
+declare var google: any;
+
+interface Marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
+
+interface Location {
+  lat: number;
+  lng: number;
+  viewport?: Object;
+  zoom: number;
+  address_level_1?: string;
+  address_level_2?: string;
+  address_country?: string;
+  address_zip?: string;
+  address_state?: string;
+  marker?: Marker;
+}
 
 @Component({
-  selector: 'app-google-map',
-  templateUrl: './google-map.component.html',
-  styleUrls: ['./google-map.component.css']
+  selector: "app-google-map",
+  templateUrl: "./google-map.component.html",
+  styles: ["agm-map { height: 300px; /* height is required */ }"]
 })
 export class GoogleMapComponent implements OnInit {
+  geocoder: any;
+  public location: Location = {
+    lat: 51.678418,
+    lng: 7.809007,
+    marker: {
+      lat: 51.678418,
+      lng: 7.809007,
+      draggable: true
+    },
+    zoom: 5
+  };
 
-  constructor() { }
+  @ViewChild(AgmMap, { static: true }) map: AgmMap;
 
-  ngOnInit() {
+  constructor(
+    public mapsApiLoader: MapsAPILoader,
+    private zone: NgZone,
+    private wrapper: GoogleMapsAPIWrapper
+  ) {
+    this.mapsApiLoader = mapsApiLoader;
+    this.zone = zone;
+    this.wrapper = wrapper;
+    this.mapsApiLoader.load().then(() => {
+      this.geocoder = new google.maps.Geocoder();
+    });
   }
 
+  ngOnInit() {
+    this.location.marker.draggable = true;
+  }
 }
